563ad5ae9024a673170ad92e65df2f79875c39e1
SomyaPadhy4501 <aryanpadhy40501@gmail.com>
2024-11-14 23:59:04 -0500

    validation half

--- signLanguage/constant/training_pipeline/__init__.py
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -12,3 +12,14 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
 DATA_DOWNLOAD_URL: str = "https://github.com/SomyaPadhy4501/dataset/raw/refs/heads/main/sign%20lang%20data.zip"
+
+
+"""
+Data validation related constant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train" , "test" , "data.yaml"]
