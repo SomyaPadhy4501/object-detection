5aa92da334ef97c9be5cd0d9b5431906bca0587a
SomyaPadhy4501 <aryanpadhy40501@gmail.com>
2024-11-14 22:59:58 -0500

    data ingestion complete

---
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+ARTIFACTS_DIR:str = "artifacts"
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
+
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
+DATA_DOWNLOAD_URL: str = "https://github.com/SomyaPadhy4501/dataset/raw/refs/heads/main/sign%20lang%20data.zip"
---
+++ signLanguage/pipeline/training_pipeline.py
@@ -0,0 +1,45 @@
+import sys
+import os
+from signLanguage.logger import logging
+from signLanguage.exception import SignException
+from signLanguage.components.data_ingestion import DataIngestion
+
+
+from signLanguage.entity.config_entity import (DataIngestionConfig)
+from signLanguage.entity.artifacts_entity import (DataIngestionArtifact)
+
+class TrainPipeline:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+    
+
+    def start_data_ingestion(self) -> DataIngestionArtifact:
+        try:
+            logging.info(
+                "Entered the staret data ingestion method of Training pipeline class" 
+            )
+            logging.info("Getting data from URL")
+
+            data_ingestion = DataIngestion(
+                data_ingestion_config= self.data_ingestion_config
+            )
+
+            data_ingestion_artifact = data_ingestion.inititate_data_ingestion()
+            logging.info("Got data from URL")
+            logging.info(
+                "Exited the start data_ingestion method of Training pipeline class"
+            )
+
+            return data_ingestion_artifact
+
+        except Exception as e:
+            raise SignException(e , sys)
+
+
+    def run_pipeline(self) -> None:
+        try:
+            data_ingestion_artifact = self.start_data_ingestion()
+
+        except Exception as e:
+            raise SignException(e , sys)
+                
