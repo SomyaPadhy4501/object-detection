b9262965632411404485ce69bc711cfaf78316f6
SomyaPadhy4501 <aryanpadhy40501@gmail.com>
2024-11-17 01:37:22 -0500

    jenkins scripting setup

---
+++ scripts/jenkins.sh
@@ -0,0 +1,36 @@
+
+
+sudo apt update 
+
+sudo apt install openjdk-8-jdk -y
+
+https://pkg.jenkins.io/
+https://pkg.jenkins.io/debian-stable/
+
+sudo systemctl start jenkins
+
+sudo systemctl enable jenkins
+
+sudo systemctl status jenkins
+
+
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+sudo sh get-docker.sh
+
+sudo usermod -aG docker $USER
+
+sudo usermod -aG docker jenkins
+
+
+newgrp docker
+
+sudo apt install awscli -y
+
+sudo usermod -a -G docker jenkins
+
+aws configure
+
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
